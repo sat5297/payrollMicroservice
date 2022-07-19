d26ecb9d009447e2c8b98736c373e815eed73b2a
Shakti <shaktiratan.5297@gmail.com>
2022-07-19 22:17:14 +0530

    Ammended Routes

--- routes/payroll.js
+++ routes/payroll.js
@@ -7,14 +7,17 @@ router.route('/issuePayCheck')
 
 router.route('/allEmp')
         .post(payrollController.allEmp)
+        .get(payrollController.allEmp)
 
 router.route('/addEmp')
         .post(payrollController.addEmp)
 
 router.route('/delete')
         .post(payrollController.deleteEmp)
+        .delete(payrollController.deleteEmp)
 
 router.route('/update')
         .post(payrollController.updateEmp)
+        .patch(payrollController.updateEmp)
 
 module.exports = router;
